23dece35d5a3e4fde91b4cc4efc6e1da9bbb139f
minmouse99 <threeminh@gmail.com>
2022-07-26 16:52:52 +0900

    Manual_Sequence

--- Assets/Scripts/Tutorial Pannel_Scripts/TXT_StepNUM.cs
+++ Assets/Scripts/Tutorial Pannel_Scripts/TXT_StepNUM.cs
@@ -9,6 +9,7 @@ public class TXT_StepNUM : MonoBehaviour
     public TextMeshProUGUI txt_StepNum;
 
     void Update(){
+        StepNumber = GameObject.Find("Tutorial Pannel").GetComponent<EducationMode>().step;
         txt_StepNum.text="STEP:"+StepNumber;
     }
 }
