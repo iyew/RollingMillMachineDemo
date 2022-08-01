0597aff185c026ba1c8ece36d6c40cbdef1038e2
minmouse99 <threeminh@gmail.com>
2022-08-01 18:29:39 +0900

    0801_final

--- Assets/Scripts/Sequence/Mode.cs
+++ Assets/Scripts/Sequence/Mode.cs
@@ -30,28 +30,27 @@ public class Mode : MonoBehaviour
       Nume  = machineController.GetComponent<ControlllerStep>().num;
       Action= machineController.GetComponent<ControlllerStep>().action;
 
-      Debug.Log("움직임:"+Nume);
 
-      if(step==1 && state[step-1]==1)      {step++; Action= 0;}
-      else if(step==2 && state[step-1]==1) {step++; Action= 0;}
-      else if(step==3 && state[step-1]==0) {step++; Action= 0;}
-      else if(step==4 && state[step-1]==1) {step++; Action= 0;}
-      else if(step==5 && state[step-1]==0) {step++; Action= 0;}
-      else if(step==6 && state[step-1]==0) {step++; Action= 0;}
-      else if(step==7 && state[step-1]==1) {step++; Action= 0;}
-      else if(step==8 && state[step-1]==0) {step++; Action= 0;}
-      else if(step==9)                     {step++; Action= 0;}
+      if(step==1 && state[step-1]==1)      {step++; Action= 0; state_copy = state;}
+      else if(step==2 && state[step-1]==1) {step++; Action= 0; state_copy = state;}
+      else if(step==3 && state[step-1]==0) {step++; Action= 0; state_copy = state;}
+      else if(step==4 && state[step-1]==1) {step++; Action= 0; state_copy = state;}
+      else if(step==5 && state[step-1]==0) {step++; Action= 0; state_copy = state;}
+      else if(step==6 && state[step-1]==0) {step++; Action= 0; state_copy = state;}
+      else if(step==7 && state[step-1]==1) {step++; Action= 0; state_copy = state;}
+      else if(step==8 && state[step-1]==0) {step++; Action= 0; state_copy = state;}
+      else if(step==9)                     {step++; Action= 0; state_copy = state;}
       else{
+        Debug.Log("state_copy:"+state_copy+"state:"+state);
         Debug.Log("Wrong!!!"+ Nume +":"+state[Nume]+"->"+state_copy[Nume]);
         state[Nume] = state_copy[Nume];
         
         time +=Time.deltaTime;
         if(time<1.0f) {Environment_light.GetComponent<Light>().color = Color.red; /*System.Media.SystemSounds.Beep.Play();*/}
-        else {time = 0; Environment_light.GetComponent<Light>().color = Color.white; machineController.GetComponent<ControlllerStep>().action = 0;} 
+        else {time = 0; Environment_light.GetComponent<Light>().color = Color.white; machineController.GetComponent<ControlllerStep>().action = 0; state_copy = state;} 
 
       }
       Debug.Log(state[0]*10000000+state[1]*1000000+state[2]*100000+state[3]*10000+state[4]*1000+state[5]*100+state[6]*10+state[7]);
     }
-    state_copy = state;
   }
 }
