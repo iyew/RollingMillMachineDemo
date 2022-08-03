4dd7f299272067e1d8ae53c5ac3d4512df63e65f
minmouse99 <threeminh@gmail.com>
2022-08-04 02:24:27 +0900

    End_Tada
    -26step(끝까지) Tutorial Mode 가능
    **Manual Mode시 틀린 동작 되돌리는 기능 추가해야 함

--- Assets/Scripts/Sequence/ControlllerStep.cs
+++ Assets/Scripts/Sequence/ControlllerStep.cs
@@ -10,8 +10,10 @@ public class ControlllerStep : MonoBehaviour
     public const int Reverse = 1;
     public const int Up      = 0;
     public const int Down    = 1;
+    public const int Mount = 0;
+    public const int UnMOunt = 1;
 
-    public const int RFCy    = 0;
+    public const int RFCy    = 0; //W/R
     public const int WRBCy   = 1;
     public const int SpSup   = 2;
     public const int WRCh    = 3;
@@ -19,45 +21,71 @@ public class ControlllerStep : MonoBehaviour
     public const int MomCar  = 5;
     public const int WRC     = 6;
     public const int Babycar = 7;
-    public const int BURCH   = 8;
-    public const int BURCC   = 9;
+    public const int BURBCY  = 8;  //BUR
+    public const int BURCH   = 9;
+    public const int BURC    = 10;
+    public const int BURCC   = 11;
+    public const int WRCrane = 12;  //Crane
+    public const int ZigCrane= 13;
+    public const int BURCrane=14; 
 
-    public int[] MenuState;
+
+    public int[] State;
+    public int[] Crane;
     public int action;
 
     void Start(){
         action = 0;
-        MenuState = new int[10];
-        for(int i=0; i<10; i++) MenuState[i]=2;
+        State = new int[15];
+        for(int i=0; i<15; i++) State[i]=2;
+
+        Debug.Log(State.LongLength);
     }
 
-    public void RFCyUp()  {MenuState[RFCy]= Up;                num=0; action=1;}
-    public void RFCyDown(){MenuState[RFCy]= Down;              num=0; action=1;}
+    public void RFCyUp()         {State[RFCy]= Up;           num=0; action=1;}
+    public void RFCyDown()       {State[RFCy]= Down;         num=0; action=1;}
 
-    public void WRBCyUp()  {MenuState[WRBCy]= Up;              num=1; action=1;} 
-    public void WRBCyDown(){MenuState[WRBCy]= Down;            num=1; action=1;}
+    public void WRBCyUp()        {State[WRBCy]= Up;          num=1; action=1;} 
+    public void WRBCyDown()      {State[WRBCy]= Down;        num=1; action=1;}
 
-    public void SpSupClamp()  {MenuState[SpSup]= Clamp;        num=2; action=1;}
-    public void SpSupUnClamp(){MenuState[SpSup]= UnClamp;      num=2; action=1;}
+    public void SpSupClamp()     {State[SpSup]= Clamp;       num=2; action=1;}
+    public void SpSupUnClamp()   {State[SpSup]= UnClamp;     num=2; action=1;}
     
-    public void WRChClamp()   {MenuState[WRCh]= Clamp;         num=3; action=1;}
-    public void WRChUnClamp() {MenuState[WRCh]= UnClamp;       num=3; action=1;}
+    public void WRChClamp()      {State[WRCh]= Clamp;        num=3; action=1;}
+    public void WRChUnClamp()    {State[WRCh]= UnClamp;      num=3; action=1;}
 
-    public void RCCForward()  {MenuState[RCC]= Forward;        num=4; action=1;}
-    public void RCCBackward() {MenuState[RCC]= Reverse;        num=4; action=1;}
+    public void RCCForward()     {State[RCC]= Forward;       num=4; action=1;}
+    public void RCCBackward()    {State[RCC]= Reverse;       num=4; action=1;}
 
-    public void MomCarForward() {MenuState[MomCar]= Forward;   num=5; action=1;}
-    public void MomCarBackward(){MenuState[MomCar]= Reverse;   num=5; action=1;}
+    public void MomCarForward()  {State[MomCar]= Forward;    num=5; action=1;}
+    public void MomCarBackward() {State[MomCar]= Reverse;    num=5; action=1;}
 
-    public void WRCClamp()  {MenuState[WRC]= Clamp;            num=6; action=1;}
-    public void WRCUnClamp(){MenuState[WRC]= UnClamp;          num=6; action=1;}
+    public void WRCClamp()       {State[WRC]= Clamp;         num=6; action=1;}
+    public void WRCUnClamp()     {State[WRC]= UnClamp;       num=6; action=1;}
     
-    public void BabycarForward() {MenuState[Babycar]= Forward; num=7; action=1;}
-    public void BabycarBackward(){MenuState[Babycar]= Reverse; num=7; action=1;}
+    public void BabycarForward() {State[Babycar]= Forward;   num=7; action=1;}
+    public void BabycarBackward(){State[Babycar]= Reverse;   num=7; action=1;}
+    public void BabycarOut()     {State[Babycar]= 3;         num=7; action=1;}
 
-    public void BURCHClamp()     {MenuState[BURCH]=Clamp; num=8; action=1;}
-    public void BURCHUnClamp()   {MenuState[BURCH]=UnClamp; num=8; action=1;}
+    public void BURBCyUp()       {State[BURBCY]=Up;           num=8; action=1;}
+    public void BURCBCyDown()    {State[BURBCY]=Down;         num=8; action=1;}
 
-    public void BURCCForward()  {MenuState[BURCC]= Forward; num=9; action=1;}
-    public void BURCCBackward() {MenuState[BURCC]= Reverse; num=9; action=1;}
+    public void BURCHClamp()     {State[BURCH]=Clamp;        num=9; action=1;}
+    public void BURCHUnClamp()   {State[BURCH]=UnClamp;      num=9; action=1;}
+
+    public void BURCCForward()   {State[BURCC]= Forward;     num=10; action=1;}
+    public void BURCCBackward()  {State[BURCC]= Reverse;     num=10; action=1;}
+
+    public void BURCClamp()      {State[BURC]= Clamp;        num=11; action=1;}
+    public void BURCUnClamp()    {State[BURC]= UnClamp;      num=11; action=1;}
+
+
+    public void WRCraneMount()    {State[WRCrane]= Mount;    num=12; action=1;}
+    public void WRCraneUnMount()  {State[WRCrane]= UnMOunt;  num=12; action=1;}
+
+    public void ZigCraneMount()   {State[ZigCrane]= Mount;   num=13; action=1;}
+    public void ZigCraneUnMount() {State[ZigCrane]= UnMOunt; num=13; action=1;}
+    
+    public void BURCraneMount()   {State[BURCrane]= Mount;   num=14; action=1;}
+    public void BURCraneUnMount() {State[BURCrane]= UnMOunt; num=14; action=1;}
 }
